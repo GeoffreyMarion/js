69c13fc8623efa8705c60255d67c82dff6bbfd1b
Geoffrey <geoffrey.marion.pro@gmail.com>
2022-04-26 16:59:05 +0200

    26/04 ajax

---
+++ Ajax/exo2/app.js
@@ -0,0 +1,49 @@
+$.ajax({
+    dataType: "JSON",
+    url: "base.json",
+    success: function(data) {
+        contente="";
+        contentv="";
+        contenti="";
+        /*$.each(data.etudiants,function(index,value){
+            contente += "<li>"+value.nom+" "+value.prenom+" "+value.moyenne+"</li>";
+            $("#etudiants").html(contente)
+
+            if(value.moyenne>=10){
+                contentv += "<li>"+value.nom+" "+value.prenom+" "+value.moyenne+"</li>";
+                $("#valides").html(contentv)
+                }
+            else{
+                contenti += "<li>"+value.nom+" "+value.prenom+" "+value.moyenne+"</li>";
+                $("#invalides").html(contenti)
+                }
+        })*/
+        $.each(data.etudiants,function(index,value){
+            contente += "<tr>"+"<td>"+value.nom+"</td>"+"<td>"+value.prenom+"</td>"+"<td>"+value.moyenne+"</td>"+"</tr>";
+            $("#etudiants").html(contente)
+
+            if(value.moyenne>=10){
+                contentv += "<tr>"+"<td>"+value.nom+"</td>"+"<td>"+value.prenom+"</td>"+"<td>"+value.moyenne+"</td>"+"</tr>";
+                $("#valides").html(contentv)
+                }
+            else{
+                contenti += "<tr>"+"<td>"+value.nom+"</td>"+"<td>"+value.prenom+"</td>"+"<td>"+value.moyenne+"</td>"+"</tr>";
+                $("#invalides").html(contenti)
+                }
+        })
+    },
+    error:function(xhr){
+        console.log(xhr.status);
+    }
+});
+//teste de bouton pour organiser les valeurs du tableau
+$("button#moyenne").click(function(){
+    let tetudiants=$("table#etudiants");
+    let tretudiants=$("table#etudiants tr");
+    let tdetudiants=$("table#etudiants td");
+    console.log(tretudiants);
+    
+    $.each(tetudiants,function(etudiants,tretudiants){
+        
+    });
+});
